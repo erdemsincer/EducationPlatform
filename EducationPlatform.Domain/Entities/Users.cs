d918f9898369893c8c52ec982f0d32db60ec961b
Erdem <erdemsincer123@icloud.com>
2025-02-27 15:02:18 +0300

    sql bağlantısı oluşturuldu

---
+++ EducationPlatform.Domain/Entities/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace EducationPlatform.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }  // Otomatik artan birincil anahtar
+        public string FullName { get; set; }  // Kullanıcının adı ve soyadı
+        public string Email { get; set; }  // Kullanıcı e-posta adresi (Benzersiz olmalı)
+        public string PasswordHash { get; set; }  // Şifre (hashlenmiş)
+        public string ProfileImage { get; set; }  // Profil fotoğrafı URL
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;  // Kullanıcı oluşturulma tarihi
+    }
+}
--- EducationPlatform.Persistence/Context/ApplicationDbContext.cs
+++ EducationPlatform.Persistence/Context/ApplicationDbContext.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using EducationPlatform.Domain.Entities;
+using OpenTelemetry.Resources;
 
 namespace EducationPlatform.Persistence.Context
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+ 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Email sütununu benzersiz yapmak için
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
     }
 }
