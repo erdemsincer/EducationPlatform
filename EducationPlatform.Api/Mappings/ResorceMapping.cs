671e8ccef20484d586a3a66f21372d48c1811759
Erdem <erdemsincer123@icloud.com>
2025-02-27 21:59:24 +0300

    Mapping işlemi

---
+++ EducationPlatform.Api/Mappings/ResorceMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using EducationPlatform.Domain.Entities;
+using EducationPlatform.Dto.ResourceDto;
+
+namespace EducationPlatform.Api.Mappings
+{
+    public class ResourceMapping : Profile
+    {
+        public ResourceMapping()
+        {
+            CreateMap<Resource, ResultResourceDto>().ReverseMap();
+            CreateMap<Resource, UpdateResourceDto>().ReverseMap();
+            CreateMap<Resource, CreateResourceDto>().ReverseMap();
+        }
+
+    }
+}
