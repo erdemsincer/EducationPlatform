d0d3c9329814dccc33e39c8d4e5806d015284b1a
Erdem <erdemsincer123@icloud.com>
2025-02-27 23:30:16 +0300

    Admin teması parçalandı

---
+++ EducationPlatform.WebUI/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPlatform.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminCategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
