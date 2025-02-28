d31c3e1694515eab09faf5658e526c995203499c
Erdem <erdemsincer123@icloud.com>
2025-02-28 21:29:44 +0300

    resource index

---
+++ EducationPlatform.WebUI/Areas/Admin/Controllers/AdminResourceController.cs
@@ -0,0 +1,34 @@
+﻿using EducationPlatform.Dto.CategoryDto;
+using EducationPlatform.Dto.ResourceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace EducationPlatform.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/AdminResource")]
+    public class AdminResourceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminResourceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7028/api/Resource");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultResourceDto>>(jsonData);
+                return View(values);
+            }
+            return View(new List<ResultResourceDto>());
+        }
+
+    }
+}
