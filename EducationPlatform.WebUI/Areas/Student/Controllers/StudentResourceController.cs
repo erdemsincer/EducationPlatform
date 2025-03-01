c1aa35a747454207e65c71137b8d792954d402ac
Erdem <erdemsincer123@icloud.com>
2025-03-01 23:22:40 +0300

    Kaynak ekleme

--- EducationPlatform.WebUI/Areas/Student/Controllers/StudentResourceController.cs
+++ EducationPlatform.WebUI/Areas/Student/Controllers/StudentResourceController.cs
@@ -1,8 +1,10 @@
 ﻿using EducationPlatform.Dto.CategoryDto;
 using EducationPlatform.Dto.ResourceDto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using System.Text;
 
 namespace EducationPlatform.WebUI.Areas.Student.Controllers
 {
@@ -17,6 +19,32 @@ namespace EducationPlatform.WebUI.Areas.Student.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
+        private async Task LoadCategoryDropdown()
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("AuthToken"));
+
+            var response = await client.GetAsync("https://localhost:7028/api/Category");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                ViewBag.Categories = new List<SelectListItem>();
+                return;
+            }
+
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var categoryList = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+
+            List<SelectListItem> categories = categoryList.Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            }).ToList();
+
+            ViewBag.Categories = categories;
+        }
+
         [Route("Index")]
         public async Task<IActionResult> Index()
         {
@@ -26,7 +54,6 @@ namespace EducationPlatform.WebUI.Areas.Student.Controllers
             if (string.IsNullOrEmpty(userId))
             {
                 return RedirectToRoute(new { controller = "Auth", action = "Login", area = "" });
-
             }
 
             var client = _httpClientFactory.CreateClient();
@@ -46,5 +73,56 @@ namespace EducationPlatform.WebUI.Areas.Student.Controllers
 
             return View(resources);
         }
+
+        [HttpGet]
+        [Route("CreateResource")]
+        public async Task<IActionResult> CreateResource()
+        {
+            var userId = HttpContext.Session.GetString("UserId");
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToRoute(new { controller = "Auth", action = "Login", area = "" });
+            }
+
+            await LoadCategoryDropdown();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateResource")]
+        public async Task<IActionResult> CreateResource(CreateResourceDto createResourceDto)
+        {
+            var userId = HttpContext.Session.GetString("UserId");
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToRoute(new { controller = "Auth", action = "Login", area = "" });
+            }
+
+            createResourceDto.UserId = int.Parse(userId); // Kullanıcı ID ekleniyor
+
+            var client = _httpClientFactory.CreateClient();
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("AuthToken"));
+
+            var jsonData = JsonConvert.SerializeObject(createResourceDto);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("https://localhost:7028/api/Resource", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                // **📌 API'den Gelen Hata Mesajını Oku**
+                var errorMessage = await response.Content.ReadAsStringAsync();
+                ViewBag.ErrorMessage = $"API Hatası: {errorMessage}";
+
+                await LoadCategoryDropdown();
+                return View(createResourceDto);
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
