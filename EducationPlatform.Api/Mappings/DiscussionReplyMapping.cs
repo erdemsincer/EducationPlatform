b298d1f7e9bedc5192b2dbb349343a5b88efa9a1
Erdem <erdemsincer123@icloud.com>
2025-03-03 23:40:05 +0300

    mapper işlemi

---
+++ EducationPlatform.Api/Mappings/DiscussionReplyMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using EducationPlatform.Domain.Entities;
+using EducationPlatform.Dto.DiscussionDto;
+using EducationPlatform.Dto.DiscussionReplyDto;
+
+namespace EducationPlatform.Api.Mappings
+{
+    public class DiscussionReplyMapping : Profile
+    {
+        public DiscussionReplyMapping()
+        {
+            CreateMap<DiscussionReply, ResultDiscussionReplyDto>().ReverseMap();
+            CreateMap<DiscussionReply, CreateDiscussionReplyDto>().ReverseMap();
+        }
+    }
+}
