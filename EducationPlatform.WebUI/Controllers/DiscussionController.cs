7386bc267bebe65d803e71c1a07a31a1396aab61
Erdem <erdemsincer123@icloud.com>
2025-03-08 00:11:00 +0300

    son 4 tartışmalar

---
+++ EducationPlatform.WebUI/Controllers/DiscussionController.cs
@@ -0,0 +1,28 @@
+﻿using EducationPlatform.Dto.SubscriberDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EducationPlatform.WebUI.Controllers
+{
+    public class DiscussionController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DiscussionController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Subscribe(CreateSubscriberDto model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.PostAsJsonAsync("https://localhost:7028/api/Subscriber", model);
+            return NoContent();
+        }
+    }
+}
