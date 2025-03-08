9a312155c5f04f7c03adec30b9ef7f61b2062080
Erdem <erdemsincer123@icloud.com>
2025-03-08 03:29:17 +0300

    profile update

--- EducationPlatform.WebUI/Program.cs
+++ EducationPlatform.WebUI/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
+
 
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
